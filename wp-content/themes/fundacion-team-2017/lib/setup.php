a56794b7caba74bf86e9abe06d1c37d3f6ee6392
Daniel Aguilar <dacmail@gmail.com>
2018-01-04 12:01:50 +0100

    Add Polylang

--- wp-content/themes/fundacion-team-2017/lib/setup.php
+++ wp-content/themes/fundacion-team-2017/lib/setup.php
@@ -27,7 +27,9 @@ function setup() {
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage')
+    'primary_navigation' => __('Menú principal', 'ungrynerd'),
+    'secondary_navigation' => __('Menú secundario', 'ungrynerd'),
+    'language_nav' => __('Menú idiomas', 'ungrynerd'),
   ]);
 
   // Enable post thumbnails
--- wp-content/themes/fundacion-team-2017/templates/header.php
+++ wp-content/themes/fundacion-team-2017/templates/header.php
@@ -1,13 +1,20 @@
-<header class="banner">
+<header class="header">
   <div class="container">
-    <a class="menu-toggle">Menu</a>
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <a href="#menu" class="menu-toggle"><?php esc_html_e('Menu', 'ungrynerd'); ?></a>
+    <?php if (has_custom_logo()): ?>
+      <?php the_custom_logo(); ?>
+    <?php else: ?>
+      <a class="header__site-name" href="<?= esc_url(home_url('/')); ?>">
+        <?php bloginfo('name'); ?>
+      </a>
+    <?php endif ?>
+    <?php
+    if (has_nav_menu('language_nav')) :
+      wp_nav_menu(array(
+        'theme_location' => 'language_nav',
+        'menu_class' => 'nav',
+        'container_class' => 'header__language'));
+    endif;
+    ?>
   </div>
 </header>
