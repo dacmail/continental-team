8a9475c663a126bd39c90e89363b3cdd34659870
Daniel Aguilar <dacmail@gmail.com>
2018-03-12 10:14:36 +0100

    Traducciones

--- wp-content/themes/fundacion-team-2017/404.php
+++ wp-content/themes/fundacion-team-2017/404.php
@@ -1,7 +1,8 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<section class="page-section">
+    <h1 class="section-title"><?php esc_html_e('Página no encontrada', 'ungrynerd'); ?></h1>
+    <div class="page-section__content">
+      <?php esc_html_e('La página que buscas parece que no existe.', 'ungrynerd'); ?>
+    </div>
+</section>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
 
-<?php get_search_form(); ?>
