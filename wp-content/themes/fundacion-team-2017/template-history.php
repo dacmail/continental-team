7c3af046824a73bf4a98cce1b513a721176d391d
Daniel Aguilar <dacmail@gmail.com>
2018-12-18 11:27:02 +0100

    Historia

--- wp-content/themes/fundacion-team-2017/template-history.php
+++ wp-content/themes/fundacion-team-2017/template-history.php
@@ -11,59 +11,63 @@
     <?php if (have_rows('years')) : ?>
       <ul class="history__yearlist">
         <?php while (have_rows('years')) : the_row(); ?>
-          <li class="history__yearlist-item"><?php the_sub_field('year'); ?></li>
+          <li class="history__yearlist-item"><a href="#year-<?php the_sub_field('year'); ?>"><?php the_sub_field('year'); ?></a></li>
         <?php endwhile; ?>
       </ul>
 
 
       <div class="history__years">
         <?php while (have_rows('years')) : the_row(); ?>
-          <div class="history__year">
+          <div class="history__year" id="year-<?php the_sub_field('year'); ?>">
+
             <div class="history__year-intro">
-              <h2 class="history__year-title">
-                <?php esc_html_e('Temporada', 'ungrynerd'); ?> <?php the_sub_field('year'); ?>
-              </h2>
-              <div class="history__year-content">
-                <?php the_sub_field('content'); ?>
+              <div class="container">
+                <h2 class="history__year-title">
+                  <?php esc_html_e('Temporada', 'ungrynerd'); ?> <strong><?php the_sub_field('year'); ?></strong>
+                </h2>
+                <div class="history__year-content">
+                  <?php the_sub_field('content'); ?>
+                </div>
+                <?php $team_photo = get_sub_field('team_photo'); ?>
+                <?= wp_get_attachment_image($team_photo, 'featured', false, array('class' => 'history__year-photo')) ?>
               </div>
-              <?php $team_photo = get_sub_field('team_photo'); ?>
-              <?= wp_get_attachment_image($team_photo, 'featured', false, array('class' => 'history__year-photo')) ?>
             </div>
 
-          </div>
-
-          <div class="history__info">
-            <?php if (have_rows('riders')) : ?>
+            <div class="history__info">
+              <?php if (have_rows('riders')) : ?>
+                <div class="history__info-block">
+                  <h3 class="history__info-title"><?php esc_html_e('Corredores', 'ungrynerd'); ?></h3>
+                  <ul class="history__riders">
+                  <?php while (have_rows('riders')) : the_row(); ?>
+                    <li class="history__rider">
+                      <?= Extras\ungrynerd_svg('flag-' . get_sub_field('bandera')) ?> <?php the_sub_field('name'); ?>
+                    </li>
+                  <?php endwhile; ?>
+                  </ul>
+                </div>
+              <?php endif ?>
+              <?php if (have_rows('results')) : ?>
+                <div class="history__info-block">
+                  <h3 class="history__info-title"><?php esc_html_e('Palmarés', 'ungrynerd'); ?></h3>
+                  <ul class="history__results">
+                  <?php while (have_rows('results')) : the_row(); ?>
+                    <li class="history__result">
+                      <strong><?php the_sub_field('posicion'); ?></strong>
+                      <?php the_sub_field('carrera'); ?>
+                    </li>
+                  <?php endwhile; ?>
+                  </ul>
+                </div>
+              <?php endif ?>
               <div class="history__info-block">
-                <h3 class="history__info-title"><?php esc_html_e('Corredores', 'ungrynerd'); ?></h3>
-                <ul class="history_riders">
-                <?php while (have_rows('riders')) : the_row(); ?>
-                  <li class="history__rider">
-                    <?= Extras\ungrynerd_svg('flag-' . get_sub_field('bandera')) ?> <?php the_sub_field('name'); ?>
-                  </li>
-                <?php endwhile; ?>
-                </ul>
+                <h3 class="history__info-title"><?php esc_html_e('Maillot', 'ungrynerd'); ?></h3>
+                <?php $maillot = get_sub_field('maillot'); ?>
+                <?= wp_get_attachment_image($maillot, 'masonry', false, array('class' => 'history__maillot')) ?>
               </div>
-            <?php endif ?>
-            <?php if (have_rows('results')) : ?>
-              <div class="history__info-block">
-                <h3 class="history__info-title"><?php esc_html_e('Palmarés', 'ungrynerd'); ?></h3>
-                <ul class="history_results">
-                <?php while (have_rows('results')) : the_row(); ?>
-                  <li class="history__result">
-                    <strong><?php the_sub_field('posicion'); ?></strong>
-                    <?php the_sub_field('carrera'); ?>
-                  </li>
-                <?php endwhile; ?>
-                </ul>
-              </div>
-            <?php endif ?>
-            <div class="history__info-block">
-              <h3 class="history__info-title"><?php esc_html_e('Maillot', 'ungrynerd'); ?></h3>
-              <?php $maillot = get_sub_field('maillot'); ?>
-              <?= wp_get_attachment_image($maillot, 'masonry', false, array('class' => 'history__maillot')) ?>
             </div>
           </div>
+
+
         <?php endwhile; ?>
 
       </div>
