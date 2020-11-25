0dd17528f5642d75009b446c5617ea3069ebace5
Daniel Aguilar <dacmail@gmail.com>
2020-11-25 17:22:39 +0100

    Añadido campo procyclingstats

--- wp-content/themes/fundacion-team-2017/templates/content-single-rider.php
+++ wp-content/themes/fundacion-team-2017/templates/content-single-rider.php
@@ -1,4 +1,6 @@
-<?php use Roots\Sage\Extras; ?>
+<?php
+
+use Roots\Sage\Extras; ?>
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header class="rider__header">
@@ -7,18 +9,18 @@
           <?php if (get_field('video')) : ?>
             <video class="rider__video" src="<?php the_field('video') ?>" autoplay="true" muted></video>
           <?php else : ?>
-              <?php the_post_thumbnail('rider-big'); ?>
+            <?php the_post_thumbnail('rider-big'); ?>
           <?php endif; ?>
         </div>
         <div class="rider__data">
           <div class="rider__social">
-            <?php if ( get_field('twitter') ): ?>
+            <?php if (get_field('twitter')) : ?>
               <a target="_blank" href="<?php the_field('twitter') ?>"><?= Extras\ungrynerd_svg('icon-twitter-rider') ?></a>
             <?php endif; ?>
-            <?php if ( get_field('instagram') ): ?>
+            <?php if (get_field('instagram')) : ?>
               <a target="_blank" href="<?php the_field('instagram') ?>"><?= Extras\ungrynerd_svg('icon-instagram-rider') ?></a>
             <?php endif; ?>
-            <?php if ( get_field('facebook') ): ?>
+            <?php if (get_field('facebook')) : ?>
               <a target="_blank" href="<?php the_field('facebook') ?>"><?= Extras\ungrynerd_svg('icon-facebook-rider') ?></a>
             <?php endif; ?>
           </div>
@@ -26,7 +28,7 @@
           <div class="rider__info">
             <div class="rider__info__box" data-birth="<?php the_field('fecha-nacimiento') ?>">
               <span><?php esc_html_e('Edad', 'ungrynerd'); ?></span>
-              <?= floor((time() - strtotime(str_replace('/', '-', get_field('fecha-nacimiento')))) / (60*60*24*365)) ?>
+              <?= floor((time() - strtotime(str_replace('/', '-', get_field('fecha-nacimiento')))) / (60 * 60 * 24 * 365)) ?>
             </div>
             <div class="rider__info__box">
               <span><?php esc_html_e('Altura (cm)', 'ungrynerd'); ?></span>
@@ -46,58 +48,64 @@
         <?php the_content(); ?>
       </div>
       <div class="rider__results">
-      <h2 class="rider__title"><?php esc_html_e('Palmarés', 'ungrynerd'); ?></h2>
-        <?php if (have_rows('palmares')): ?>
+        <h2 class="rider__title">
+          <?php esc_html_e('Palmarés', 'ungrynerd'); ?>
+          <?php if (get_field('stats')) : ?>
+            <span>(<a target="_blank" href="<?php the_field('stats') ?>">procyclingstats</a>)</span>
+          <?php endif; ?>
+        </h2>
+        <?php if (have_rows('palmares')) : ?>
           <ul class="rider__results__list">
-          <?php while (have_rows('palmares')) : the_row(); ?>
-            <li class="rider__results__year"><?php the_sub_field('year'); ?>
-            <?php if (have_rows('rows')): ?>
-              <ul>
-              <?php while (have_rows('rows')) : the_row(); ?>
-                <li class="rider__results__result">
-                  <strong><?php the_sub_field('posicion'); ?></strong>
-                  <?php the_sub_field('carrera'); ?>
-                </li>
-              <?php endwhile; ?>
-              </ul>
-            <?php endif ?>
-            </li>
-          <?php endwhile; ?>
+            <?php while (have_rows('palmares')) : the_row(); ?>
+              <li class="rider__results__year"><?php the_sub_field('year'); ?>
+                <?php if (have_rows('rows')) : ?>
+                  <ul>
+                    <?php while (have_rows('rows')) : the_row(); ?>
+                      <li class="rider__results__result">
+                        <strong><?php the_sub_field('posicion'); ?></strong>
+                        <?php the_sub_field('carrera'); ?>
+                      </li>
+                    <?php endwhile; ?>
+                  </ul>
+                <?php endif ?>
+              </li>
+            <?php endwhile; ?>
           </ul>
         <?php endif ?>
       </div>
     </div>
     <?php $images = get_field('galeria'); ?>
-    <?php if ( $images ): ?>
-      <ul class="rider__gallery" >
-        <?php foreach( $images as $image ): ?>
+    <?php if ($images) : ?>
+      <ul class="rider__gallery">
+        <?php foreach ($images as $image) : ?>
           <li class="rider__gallery__item">
-            <?= wp_get_attachment_image($image['ID'], 'rider-gallery' ); ?>
+            <?= wp_get_attachment_image($image['ID'], 'rider-gallery'); ?>
           </li>
         <?php endforeach; ?>
       </ul>
     <?php endif; ?>
     <?php $news = new WP_Query(array(
       'post_type' => array('post'),
-      'meta_query'		=> array(
-                          array(
-                            'key' => 'riders',
-                            'value' => '"' . get_the_ID() . '"',
-                            'compare' => 'LIKE'
-                            )
-                          ),
-      'posts_per_page' => 3)) ?>
-    <?php if ($news->have_posts()): ?>
+      'meta_query'    => array(
+        array(
+          'key' => 'riders',
+          'value' => '"' . get_the_ID() . '"',
+          'compare' => 'LIKE'
+        )
+      ),
+      'posts_per_page' => 3
+    )) ?>
+    <?php if ($news->have_posts()) : ?>
       <div class="rider__news">
         <h2 class="rider__title"><?php esc_html_e('Noticias relacionadas', 'ungrynerd'); ?></h2>
         <div class="rider__news__wrapper">
-        <?php while ($news->have_posts()) : $news->the_post(); ?>
-          <article class="news-item">
-            <div class="news-item__image"><?php the_post_thumbnail('featured_medium') ?></div>
-            <p class="news-item__meta"><strong><?php esc_html_e('Noticia', 'ungrynerd'); ?></strong> · <?php the_time(get_option('date_format')); ?></p>
-            <h3 class="news-item__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-          </article>
-        <?php endwhile; ?>
+          <?php while ($news->have_posts()) : $news->the_post(); ?>
+            <article class="news-item">
+              <div class="news-item__image"><?php the_post_thumbnail('featured_medium') ?></div>
+              <p class="news-item__meta"><strong><?php esc_html_e('Noticia', 'ungrynerd'); ?></strong> · <?php the_time(get_option('date_format')); ?></p>
+              <h3 class="news-item__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            </article>
+          <?php endwhile; ?>
         </div>
       </div>
     <?php endif ?>
