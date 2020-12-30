437c15a80885cf18f97c72b65ef919cd688f871c
Daniel Aguilar <dacmail@gmail.com>
2020-12-30 15:23:49 +0100

    sponsors y traducciones

--- wp-content/themes/fundacion-team-2017/template-sponsors.php
+++ wp-content/themes/fundacion-team-2017/template-sponsors.php
@@ -15,15 +15,11 @@ use Roots\Sage\Extras; ?>
       <?php while ($sponsors->have_posts()) : $sponsors->the_post(); ?>
         <div class="sponsors-block sponsors-block--level1 type-<?= $sponsors->current_post; ?>">
           <div class="container">
-            <?php if ($sponsors->current_post == 0) : ?>
-              <h2 class="sponsors-block__title"><?php esc_html_e('Title Sponsors', 'ungrynerd'); ?></h2>
-            <?php endif; ?>
-
             <div class="sponsors-block__wrapper">
               <?php $logo = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full');  ?>
               <a target="_blank" class="sponsors-block__item" href="<?php the_field('sponsor_link'); ?>"><img src="<?= $logo[0]; ?>" width="<?= $logo[1] / 2; ?>" alt="<?php the_title_attribute(); ?>"></a>
               <div class="sponsors-block__text">
-                <?php the_field('text') ?>
+                <?php the_field('text_' . ICL_LANGUAGE_CODE) ?>
               </div>
               <div class="sponsors-block__social">
                 <?php $link = get_field('sponsor_link'); ?>
@@ -58,14 +54,14 @@ use Roots\Sage\Extras; ?>
       <?php while ($sponsors->have_posts()) : $sponsors->the_post(); ?>
         <div class="sponsors-block sponsors-block--level1 sponsors-block--gold">
           <?php if ($sponsors->current_post == 0) : ?>
-            <h2 class="sponsors-block__title"><?php esc_html_e('Gold', 'ungrynerd'); ?></h2>
+            <h2 class="sponsors-block__title"><?php esc_html_e('Gold sponsors', 'ungrynerd'); ?></h2>
           <?php endif; ?>
           <div class="container">
             <div class="sponsors-block__wrapper">
               <?php $logo = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full');  ?>
               <a target="_blank" class="sponsors-block__item" href="<?php the_field('sponsor_link'); ?>"><img src="<?= $logo[0]; ?>" width="<?= $logo[1] / 2; ?>" alt="<?php the_title_attribute(); ?>"></a>
               <div class="sponsors-block__text">
-                <?php the_field('text') ?>
+                <?php the_field('text_' . ICL_LANGUAGE_CODE) ?>
               </div>
               <div class="sponsors-block__social">
                 <?php $link = get_field('sponsor_link'); ?>
@@ -98,7 +94,7 @@ use Roots\Sage\Extras; ?>
     <?php $sponsors = new WP_Query(array('post_type' => array('sponsor'), 'meta_key' => 'sponsor_type', 'meta_value' => 'silver', 'posts_per_page' => -1, 'order' => 'ASC')); ?>
     <?php if ($sponsors->have_posts()) : ?>
       <div class="sponsors-block sponsors-block--level2">
-        <h2 class="sponsors-block__title"><?php esc_html_e('Silver', 'ungrynerd'); ?></h2>
+        <h2 class="sponsors-block__title"><?php esc_html_e('Silver sponsors', 'ungrynerd'); ?></h2>
       </div>
       <div class="sponsors-block sponsors-block--level2">
         <div class="container">
@@ -112,10 +108,10 @@ use Roots\Sage\Extras; ?>
 
     <?php $sponsors = new WP_Query(array('post_type' => array('sponsor'), 'meta_key' => 'sponsor_type', 'meta_value' => 'bronze', 'posts_per_page' => -1, 'order' => 'ASC')); ?>
     <?php if ($sponsors->have_posts()) : ?>
-      <div class="sponsors-block sponsors-block--level2">
-        <h2 class="sponsors-block__title"><?php esc_html_e('Bronze', 'ungrynerd'); ?></h2>
+      <div class="sponsors-block sponsors-block--level2 sponsors-block--level23">
+        <h2 class="sponsors-block__title"><?php esc_html_e('Bronze sponsors', 'ungrynerd'); ?></h2>
       </div>
-      <div class="sponsors-block sponsors-block--level2">
+      <div class="sponsors-block sponsors-block--level2 sponsors-block--level23">
         <div class="container">
           <?php while ($sponsors->have_posts()) : $sponsors->the_post(); ?>
             <?php $logo = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full');  ?>
